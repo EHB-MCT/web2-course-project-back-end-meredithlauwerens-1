3b626f376842254f2dc758750d7408fa0edb6bf4
meredithlauwerens <meredith.lauwerens@student.ehb.be>
2025-12-18 17:44:49 +0100

    updating api.js with mongoose and mongodb

--- code/ballet-exercises/api.js
+++ code/ballet-exercises/api.js
@@ -1,10 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//middleware
+app.use(express.json());
+
+//connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+//test route
 app.get("/", (req, res) => {
-  res.json({ message: "Backend is running 🚀" });
+  res.json({ message: "Backend + MongoDB connected" });
 });
 
 app.listen(PORT, () => {
