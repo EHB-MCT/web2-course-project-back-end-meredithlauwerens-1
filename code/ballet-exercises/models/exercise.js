43d1a73842ee9d5bf40ed0fce3502c0b0f97c592
meredithlauwerens <meredith.lauwerens@student.ehb.be>
2025-12-22 18:49:42 +0100

    adding mainimg to json file data

--- code/ballet-exercises/models/exercise.js
+++ code/ballet-exercises/models/exercise.js
@@ -1,31 +1,38 @@
 import mongoose from "mongoose";
 
-const stepSchema = new mongoose.Schema({
-  step: Number,
-  title: String,
-  description: String,
-  imageUrl: String
-}, { _id: false });
+const stepSchema = new mongoose.Schema(
+	{
+		step: Number,
+		title: String,
+		description: String,
+		imageUrl: String,
+	},
+	{ _id: false }
+);
 
-const textImageSchema = new mongoose.Schema({
-  text: String,
-  imageUrl: String
-}, { _id: false });
+const textImageSchema = new mongoose.Schema(
+	{
+		text: String,
+		imageUrl: String,
+	},
+	{ _id: false }
+);
 
 const exerciseSchema = new mongoose.Schema({
-  title: String,
-  category: String,
-  difficulty: Number,
+	title: String,
+	mainImg: String,
+	category: String,
+	difficulty: Number,
 
-  explanation: {
-    description: String,
-    focus: String
-  },
+	explanation: {
+		description: String,
+		focus: String,
+	},
 
-  illustrationSteps: [stepSchema],
-  tutorialVideo: String,
-  donts: [textImageSchema],
-  tips: [textImageSchema]
+	illustrationSteps: [stepSchema],
+	tutorialVideo: String,
+	donts: [textImageSchema],
+	tips: [textImageSchema],
 });
 
 export default mongoose.model("Exercise", exerciseSchema);
